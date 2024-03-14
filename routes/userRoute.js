f7490009909235b742cd634f03d8bf1b310b2d83
Stiven Aguirre Cardona <stivenalexis2005@gmail.com>
2024-03-14 12:50:25 -0500

    fiveteen and medio update

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, logoutUser } from '../controllers/userController';
+import { registerUser, loginUser, logoutUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
